6609a18c9b6d363f9e1e3af8d4c34c20604b0561
mario_alex <marioatoroh@gmail.com>
2024-03-19 18:21:38 -0300

    Agregar archivos de proyecto.

---
+++ APIBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Agregar Modelos
+    }
+}
