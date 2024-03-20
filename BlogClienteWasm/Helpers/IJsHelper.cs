4da984fbe255a58584663a9054401489c04b7938
mario_alex <marioatoroh@gmail.com>
2024-03-20 12:05:40 -0300

    Agredando proyectos blazor_wasm y configurandolo

---
+++ BlogClienteWasm/Helpers/IJsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlogClienteWasm.Helpers
+{
+    public static class IJsHelper
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ BlogClienteWasm/wwwroot/js/common.js
@@ -0,0 +1,33 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operación Correcta", { timeOut: 10000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operación Fallida", { timeOut: 10000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        );
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        );
+    }
+}
+
+function MostrarModalConfirmacionBorrado() {
+    $('#modalConfirmacionBorrado').modal('show');
+}
+
+function OcultarModalConfirmacionBorrado() {
+    $('#modalConfirmacionBorrado').modal('hide');
+}
